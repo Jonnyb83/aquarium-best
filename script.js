203ac6624f3acbbf5ad9da7e974ef6c68932b1f1
Jonnyb83 <64705009+Jonnyb83@users.noreply.github.com>
2023-02-05 14:18:26 +0100

    NavBar

--- script.js
+++ script.js
@@ -53,129 +53,24 @@ window.onclick = function(event) {
 	}
   };
 
+// Scroll up CLICK HOME
 
-// // Get the modal
-// var modal1 = document.getElementById("modal1");
+let myHome = document.getElementById("Home");
 
-// // Get the button that opens the modal
-// var btn1 = document.getElementById("btn1");
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+  };
 
-// // // Get the modal
-// // var modal2 = document.getElementById("modal2");
+// Add active class
 
-// // // Get the button that opens the modal
-// // var btn2 = document.getElementById("btn2");
-
-// // // Get the modal
-// // var modal3 = document.getElementById("modal3");
-
-// // // Get the button that opens the modal
-// // var btn3 = document.getElementById("btn3");
-// // // Get the modal
-// // var modal4 = document.getElementById("modal4");
-
-// // // Get the button that opens the modal
-// // var btn4 = document.getElementById("btn4");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on the button, open the modal
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // var Slide = function(params){
-// //   this.element = params.element;
- 
-// //   // since the options get passed through directly
-// //   // from the main instance you can use whatever
-// //   // options suit your transition
-// //   this.options = params.options;
-// // };
- 
-// // Slide.prototype.hide = function(instant, direction){
-// //   this.element.style.display = 'none';
-// // };
- 
-// // Slide.prototype.show = function(instant, direction){
-// //   this.elment.style.display = 'block';
-// // };
- 
-// // var Slides = require('slides-js');
-// // new Slides(document.querySelector('.slides'), {
-// //   transition: Slide
-// // });
-
-
-// // something else
-// // 2. This code loads the IFrame Player API code asynchronously.
-// var tag = document.createElement('script');
-
-// tag.src = "https://www.youtube.com/iframe_api";
-// var firstScriptTag = document.getElementsByTagName('script')[0];
-// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// // 3. This function creates an <iframe> (and YouTube player)
-// //    after the API code downloads.
-// var player;
-// function onYouTubeIframeAPIReady() {
-//   player = new YT.Player('player', {
-//     height: '390',
-//     width: '640',
-//     videoId: 'M7lc1UVf-VE',
-//     playerVars: {
-//       'playsinline': 1
-//     },
-//     events: {
-//       'onReady': onPlayerReady,
-//       'onStateChange': onPlayerStateChange
-//     }
-//   });
-// }
-
-// // 4. The API will call this function when the video player is ready.
-// function onPlayerReady(event) {
-//   event.target.playVideo();
-// }
-
-// // 5. The API calls this function when the player's state changes.
-// //    The function indicates that when playing a video (state=1),
-// //    the player should play for six seconds and then stop.
-// var done = false;
-// function onPlayerStateChange(event) {
-//   if (event.data == YT.PlayerState.PLAYING && !done) {
-//     setTimeout(stopVideo, 6000);
-//     done = true;
-//   }
-// }
-// function stopVideo() {
-//   player.stopVideo();
-// }
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myNav");
+var btns = header.getElementsByClassName("navLine");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
